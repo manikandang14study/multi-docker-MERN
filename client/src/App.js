18544efc5b3fdb014ac1ab1f1346f63e6582e42a
manikandang14study <manikandang14study@gmail.com>
2021-09-27 13:15:24 +0530

    Docker Hub

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ function App() {
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
+          <p>Welcome</p>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
         </div>
